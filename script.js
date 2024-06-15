ca5462afeaa93a0bd7e8b20208fbeae4a7006c1e
Shiraz Mazhar <imshiraz007@gmail.com>
2024-06-16 00:02:01 +0500

    Added Working buttons

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var start_button = document.querySelector('.start_btn');
 var quiz_info = document.querySelector('.quiz-info');
 var exit_button = document.querySelector('.exit-btn');
-container=document.querySelector('.container');
+var container=document.querySelector('.container');
+var continue_button=document.querySelector('.continue-btn');
+var quiz_section = document.querySelector('.quiz_section');
 start_button.onclick=()=>{
     quiz_info.classList.add('active')
     container.classList.add('active')
@@ -9,4 +11,10 @@ start_button.onclick=()=>{
 exit_button.onclick=()=>{
     quiz_info.classList.remove('active')
     container.classList.remove('active')
+
+}
+continue_button.onclick=()=>{
+    quiz_info.classList.remove('active')
+    container.classList.remove('active')
+    quiz_section.classList.add('active')
 }
