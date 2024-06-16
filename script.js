2d9bf93efdd5b22122ff97dde509dfe17e3827bc
Shiraz Mazhar <imshiraz007@gmail.com>
2024-06-16 12:12:46 +0500

    Added English section Mcq's

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var exit_button = document.querySelector('.exit-btn');
 var container=document.querySelector('.container');
 var continue_button=document.querySelector('.continue-btn');
 var quiz_section = document.querySelector('.quiz_section');
+var quiz_box=document.querySelector('.quiz-box');
+var next_button=document.querySelector('.next_button');
 start_button.onclick=()=>{
     quiz_info.classList.add('active')
     container.classList.add('active')
@@ -17,5 +19,15 @@ continue_button.onclick=()=>{
     quiz_info.classList.remove('active')
     container.classList.remove('active')
     quiz_section.classList.add('active')
-    
+    quiz_box.classList.add('active')
+    display_question(0)
+}
+next_button.onclick=()=>{
+    question_counter++;
+    display_question(question_counter);
+}
+var question_counter=0;
+function display_question(index){
+    var question_text=document.querySelector('.questions_text');
+    question_text.textContent=`${questions[index].q_num}.${questions[index].question}`
 }
